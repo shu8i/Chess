17d012db2c404c5c760e1eb067742b81fc23979b
Shahab Shekari <shekari.shahab@gmail.com>
2014-12-23 23:02:16 -0800

    Added some initial binary data. (incl. masks)

--- src/Chess.java
+++ src/Chess.java
@@ -2,4 +2,5 @@
  * Created by Shahab Shekari on 12/23/14.
  */
 public class Chess {
+
 }
--- src/model/Board.java
+++ src/model/Board.java
@@ -1,7 +1,18 @@
 package model;
 
+import static model.constants.Spot.*;
+import static model.constants.Board.*;
+
 /**
  * Created by Shahab Shekari on 12/23/14.
  */
 public class Board {
+
+    private long[] position = POSITION;
+
+    public Board()
+    {
+        long lolwoot = PIECE[A1.mask()];
+    }
+
 }
